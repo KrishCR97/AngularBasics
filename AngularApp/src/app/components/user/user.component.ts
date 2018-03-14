99bf060f5125fff36dd34e80d3b10c94788a0d32
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-03-14 15:02:26 -0500

    Added typescript instead of js for efficiency of the aplication.

--- AngularApp/src/app/components/user/user.component.ts
+++ AngularApp/src/app/components/user/user.component.ts
@@ -5,5 +5,23 @@ import { Component } from '@angular/core';
     styleUrls : ['./user.component.css']
 })
 export class UserComponent{
-
+//properties
+firstName : String
+lastName : String
+age : Number
+address
+//
+constructor(){
+    this.firstName = "Kishon"
+    this.lastName = "Kumar"
+    this.age = 24
+    this.address = {
+        street : 'Any Street',
+        city : 'Any city',
+        State : 'Any state'
+    }
+}
+showAge(){
+    return this.age
+}
 }
